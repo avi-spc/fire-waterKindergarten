98d305565075b6614051c2f9a12f1d1a5640bdc8
avi-spc <avdhesh7394vn@gmail.com>
2018-11-05 13:58:52 +0530

    navigation est.

---
+++ Assets/Scripts/Children.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class Children : MonoBehaviour {
+
+    NavMeshAgent agentChildren;
+
+    GameObject[] targetPoint;
+    public GameObject teacher;
+
+    public Text targetChild;
+
+    int index;
+	// Use this for initialization
+	void Start () {
+        agentChildren = GetComponent<NavMeshAgent>();
+        targetPoint = GameObject.FindGameObjectsWithTag("Fire");
+        agentChildren.speed = 0.2f;
+
+        index = Random.Range(0, targetPoint.Length);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agentChildren.SetDestination(targetPoint[index].transform.position);
+        if ((targetPoint[index].transform.position-gameObject.transform.position).magnitude<1f) {
+           // Destroy(gameObject);
+        }
+
+        if ((gameObject.transform.position - teacher.transform.position).magnitude < 0.5f && gameObject.name == targetChild.text) {
+            transform.SetParent(teacher.transform);
+            //gameObject.SetActive(false);
+            Teacher.Instance.childPicked = true;
+        }
+
+        if ((gameObject.transform.position - GameObject.FindGameObjectWithTag("Final").transform.position).magnitude<0.5f) {
+            Teacher.Instance.childPicked = false;
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Teacher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class Teacher : MonoBehaviour {
+
+    public static Teacher Instance;
+
+    NavMeshAgent agentTeacher;
+
+    public Text targetChild;
+
+    public bool childPicked;
+
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+        agentTeacher = GetComponent<NavMeshAgent>();
+        agentTeacher.speed = 4f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (childPicked) {
+            agentTeacher.SetDestination(GameObject.FindGameObjectWithTag("Final").transform.position);
+            childPicked = false;
+        }
+
+        if ((gameObject.transform.position - GameObject.FindGameObjectWithTag("Final").transform.position).magnitude < 0.8f) {
+            //agentTeacher.isStopped = true;
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            //Debug.Log((gameObject.transform.position - GameObject.FindGameObjectWithTag("Final").transform.position).magnitude);
+        }
+    }
+
+    public void Follow() {
+        //agentTeacher.isStopped = false;
+        gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        if (!childPicked) {
+            agentTeacher.SetDestination(GameObject.Find(targetChild.text).transform.position);
+        }       
+    }
+}
